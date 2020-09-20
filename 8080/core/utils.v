f39557ce7a6a1f292fbb441793344eee91869c23
trevday <day.t@northeastern.edu>
2020-09-19 18:43:46 -0700

    Correct parity calculation

--- 8080/core/utils.v
+++ 8080/core/utils.v
@@ -16,11 +16,30 @@ fn break_address(addr u16) (byte, byte) {
 	return byte((addr >> 8) & 0xff), byte(addr & 0xff)
 }
 
-fn parity(x int) bool {
-	mut temp := x ^ (x >> 1)
-	temp = temp ^ (temp >> 2)
-	temp = temp ^ (temp >> 4)
-	temp = temp ^ (temp >> 8)
-	temp = temp ^ (temp >> 16)
-	return ((temp & 1) == 1)
+fn parity(x byte) bool {
+	// 0
+	mut temp := x
+	mut p := (temp & 1)
+	// 1
+	temp = temp >> 1
+	p = p ^ (temp & 1)
+	// 2
+	temp = temp >> 1
+	p = p ^ (temp & 1)
+	// 3
+	temp = temp >> 1
+	p = p ^ (temp & 1)
+	// 4
+	temp = temp >> 1
+	p = p ^ (temp & 1)
+	// 5
+	temp = temp >> 1
+	p = p ^ (temp & 1)
+	// 6
+	temp = temp >> 1
+	p = p ^ (temp & 1)
+	// 7
+	temp = temp >> 1
+	p = p ^ (temp & 1)
+	return p == 0
 }
