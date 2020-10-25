348d264b0f00a6835ff9f3328032c9625f898daf
trevday <day.t@northeastern.edu>
2020-10-25 16:20:28 -0700

    Allow resizing of the view

--- arcade/machine/view.v
+++ arcade/machine/view.v
@@ -10,12 +10,16 @@ const (
 	view_height_pixels = 256
 	image_channels     = 4
 	framebuffer_addr   = 0x2400
+	pixel_on           = byte(255)
+	pixel_off          = byte(0)
 )
 
 struct View {
 mut:
 	framebuffer []byte
 	context     &gg.Context = voidptr(0)
+	width       int = view_width_pixels
+	height      int = view_height_pixels
 }
 
 fn new_view(mut m Machine) View {
@@ -27,6 +31,7 @@ fn new_view(mut m Machine) View {
 			height: view_height_pixels
 			use_ortho: true
 			create_window: true
+			resizable: true
 			window_title: 'V 8080'
 			user_data: m
 			frame_fn: frame
@@ -53,16 +58,16 @@ fn frame(mut m Machine) {
 	v0 := f32(0.0)
 	u1 := f32(1.0)
 	v1 := f32(1.0)
-	x0 := f32(0) * m.view.context.scale
-	y0 := f32(0) * m.view.context.scale
-	x1 := f32(view_width_pixels) * m.view.context.scale
-	y1 := f32(view_height_pixels) * m.view.context.scale
+	x0 := f32(0)
+	y0 := f32(0)
+	x1 := m.view.width * m.view.context.scale
+	y1 := m.view.height * m.view.context.scale
 	//
 	sgl.load_pipeline(m.view.context.timage_pip)
 	sgl.enable_texture()
 	sgl.texture(img.simg)
 	sgl.begin_quads()
-	sgl.c4b(255, 255, 255, 255)
+	sgl.c4b(pixel_on, pixel_on, pixel_on, pixel_on)
 	sgl.v2f_t2f(x0, y0, u0, v0)
 	sgl.v2f_t2f(x1, y0, u1, v0)
 	sgl.v2f_t2f(x1, y1, u1, v1)
@@ -86,29 +91,47 @@ fn read_framebuffer(mut m Machine) {
 			pixel := data & (1 << (bit_idx - (mem_idx * 8)))
 			out_offset := (((view_height_pixels - 1 - x) * view_width_pixels) + y) * image_channels
 			if pixel == 0 {
-				m.view.framebuffer[out_offset] = byte(0)
-				m.view.framebuffer[out_offset + 1] = byte(0)
-				m.view.framebuffer[out_offset + 2] = byte(0)
-				m.view.framebuffer[out_offset + 3] = byte(255)
+				m.view.framebuffer[out_offset] = pixel_off
+				m.view.framebuffer[out_offset + 1] = pixel_off
+				m.view.framebuffer[out_offset + 2] = pixel_off
+				m.view.framebuffer[out_offset + 3] = pixel_on
 			} else {
-				m.view.framebuffer[out_offset] = byte(255)
-				m.view.framebuffer[out_offset + 1] = byte(255)
-				m.view.framebuffer[out_offset + 2] = byte(255)
-				m.view.framebuffer[out_offset + 3] = byte(255)
+				m.view.framebuffer[out_offset] = pixel_on
+				m.view.framebuffer[out_offset + 1] = pixel_on
+				m.view.framebuffer[out_offset + 2] = pixel_on
+				m.view.framebuffer[out_offset + 3] = pixel_on
 			}
 		}
 	}
 }
 
+fn (mut v View) resize() {
+	mut s := sapp.dpi_scale()
+	if s == 0.0 {
+		s = 1.0
+	}
+	v.width = int(sapp.width() / s)
+	v.height = int(sapp.height() / s)
+}
+
 // TODO: Platform specific input code, should go in a platform specific place
 fn on_event(e &sapp.Event, mut m Machine) {
-	if e.typ == .key_down {
-		if k := map_input(e.key_code) {
-			m.io.input_down(k)
+	match e.typ {
+		.key_down {
+			if k := map_input(e.key_code) {
+				m.io.input_down(k)
+			}
 		}
-	} else if e.typ == .key_up {
-		if k := map_input(e.key_code) {
-			m.io.input_up(k)
+		.key_up {
+			if k := map_input(e.key_code) {
+				m.io.input_up(k)
+			}
+		}
+		.resized, .restored, .resumed {
+			m.view.resize()
+		}
+		else {
+			// No-op
 		}
 	}
 }
